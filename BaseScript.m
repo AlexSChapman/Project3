dd1a4ad14ac32ff64cb9951bb05fcff949f95e39
Alex Chapman <achapman@olin.edu>
2016-11-28 15:43:59 -0500

    Reverted to two figure version

--- BaseScript.m
+++ BaseScript.m
@@ -13,7 +13,7 @@ Vy = sind(theta) * Vi;   %m / s
 S = [x, y, Vx, Vy];
 
 options = odeset('Events', @events);
-[Times, Vals, eventTimes] = ode45(@MarsFlow, [0 duration] , S, options);
+[Times, Vals] = ode45(@MarsFlow, [0 duration] , S, options);
 
 %plot(Times, Vals)
 X = Vals(:,1);
@@ -21,29 +21,32 @@ Y = Vals(:,2);
 DX = Vals(:,3);
 DY = Vals(:,4);
 VELOCITY = sqrt(DX.^2 + DY.^2);
-ENERGY = 0.5 * VELOCITY.^2
-ENERGY_0 = 0.5 * 5100^2
-ERatio = ENERGY/ENERGY_0
+ENERGY = 0.5 * VELOCITY.^2;
+ENERGY_0 = 0.5 * 5100^2;
+ERatio = ENERGY/ENERGY_0;
 
-subplot(4,1,1);plot(Times,DY./1000);
+subplot(3,1,1);plot(Times,DY./1000);
 grid on;
 ylabel('Y-Speed (km/s)')
 xlabel('Time (s)')
 
-subplot(4,1,2);plot(Times,Y./1000);
+subplot(3,1,2);plot(Times,Y./1000);
 grid on;
 ylabel('Altitude (km)')
 xlabel('Time (s)')
 
-subplot(4,1,3);plot(VELOCITY./1000,Y./1000);
+subplot(3,1,3);comet(X./1000,Y./1000);
+grid on;
+ylabel('Altitude (km)')
+xlabel('X (km)')
+
+figure()
+
+plot(VELOCITY./1000,Y./1000);
 grid on;
 ylabel('Altitude (km)')
 xlabel('Velocity (km / s)')
 
-subplot(4,1,4);comet(X./1000,Y./1000);
-grid on;
-ylabel('Altitude (km)')
-xlabel('X (km)')
 
 %subplot(4,1,4);plot(Times,DX./1000);
 grid on;
@@ -55,10 +58,10 @@ xlabel('Time (s)')
 %disp(X)
 %disp(Y)
 
-T = table(Times, X./1000, Y, VELOCITY,VELOCITY *3600/1000, ERatio*100)
+T = table(Times, X./1000, Y, VELOCITY, ERatio*100);
+disp(T);
 
     function [value, isterminal,direction] = events(~,input)
-        distance = input(1);
         height = input(2);
         value = height - 0;
         isterminal = 1;
