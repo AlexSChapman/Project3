89135681198814958c9b1a5edd5b644f00f067bc
Lacie Fradet <lfradet@olin.edu>
2016-12-05 15:49:55 -0500

    jpg's of graphs and updated run code

--- run.m
+++ run.m
@@ -5,9 +5,9 @@ MaxTemp = zeros(1,length(angles));
 MaxAcc = zeros(1,length(angles));
 Vf = zeros(1,length(angles));
 dX = zeros(1,length(angles));
-C_T = 1;
-C_A = 1;
-C_Vf = 1;
+C_T = 1000;
+C_A = 1000;
+C_Vf = 1000;
 C_x = 1;
 
 for i = 1:length(angles)
@@ -19,36 +19,38 @@ for i = 1:length(angles)
     dX(i) = C_x * dX(i);
 end
 
-hold on
-plot(angles, MaxTemp);
-xlabel('Angle from Horizontal (degrees)');
-ylabel('Max Temperature (degrees Celcius)');
-title('Temperature');
+% hold on
+% plot(angles, MaxTemp);
+
 
 
 p = polyfit(angles,MaxTemp,15);
 nTemp = polyval(p,angles);
-plot(angles, nTemp);
+plot(angles, nTemp,'Linewidth',2);
+xlabel('Angle from Horizontal (degrees)');
+ylabel('Max Temperature (degrees Celcius)');
+title('Temperature');
 
 figure()
-hold on
-plot(angles,MaxAcc, 'b');
-xlabel('Angle from Horizontal (degrees)');
-ylabel('Max Acceleration (m/s^2)');
-title('Acceleration');
+% hold on
+% plot(angles,MaxAcc, 'b');
+
 
 w = polyfit(angles,MaxAcc,15);
 nAcc = polyval(w,angles);
-plot(angles, nAcc,'r');
+plot(angles, nAcc,'r','Linewidth',2);
+xlabel('Angle from Horizontal (degrees)');
+ylabel('Max Acceleration (m/s^2)');
+title('Acceleration');
 figure()
 
-plot(angles,Vf);
+ plot(angles,Vf,'Linewidth',2);
 xlabel('Angle from Horizontal (degrees)');
 ylabel('Velocity at Parachute Opening (m/s)');
 title('Final Velocity');
 figure()
 
-plot(angles,dX);
+plot(angles,dX,'Linewidth',2);
 xlabel('Angle from Horizontal (degrees)');
 ylabel('horizontal Displacement (m)');
 title('horizontalDisplacement');
@@ -56,8 +58,8 @@ figure()
 
 hold on
 quantity = MaxTemp.*MaxAcc.*Vf.*dX;
-plot(angles, quantity,'b');
-plot(angles, nTemp.*nAcc.*Vf.*dX,'r');
+% plot(angles, quantity,'b');
+plot(angles, nTemp+nAcc+Vf+dX,'r','Linewidth',2);
 
 title('Product Plot')
 xlabel('Angle from Horizontal (degrees)');
